99b6e1a5eb7223d290400c4e06c074740b245b25
geethasamynathan <geethasamynathan2011@gmail.com>
2024-10-27 20:43:22 +0530

    Added Bikestores Model and regitered in program.cs

---
+++ GitAPI1/Models/ExampleVarchar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitAPI1.Models;
+
+public partial class ExampleVarchar
+{
+    public int Id { get; set; }
+
+    public string? Description { get; set; }
+}
